212cec82ab82586817326bb0750c48eee1a3ff2c
revanth <atmakurirevanth@gmail.com>
2021-01-16 14:31:01 +0530

    today

---
+++ cppprogramming/throwaway/sim.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+	vector<char> line;
+	char letter;
+	while (cin >> letter){
+		if ((letter == "<") || (letter == "[") || (letter == "]")){
+			if (letter == "<"){
+				if ()
+			}
+			else if (letter == "[")
+		}
+	}
+}
