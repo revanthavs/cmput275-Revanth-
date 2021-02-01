d0a2d87a4880ae981cceba72717c6fa1a0aacd58
REVANTHAVS <atmakurirevanth@gamil.com>
2021-01-31 23:13:09 -0700

    1-02-2021

--- cppprogramming/main/leader.cpp
+++ cppprogramming/main/leader.cpp
@@ -1,8 +1,5 @@
-// Remember, style matters!
-// While you may rely upon additional functions 
-// ensure reverse is declared and functions as described
-#include <iostream> // this is the only header you may use (for cin, cout, and endl)
-// #include <iostream>
+
+#include <iostream>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -26,33 +23,43 @@ void reverse(int *begin, int *end){
 }
 
 int main(){
-	int n, led = 0, i = 0, arrindex = 0, count = 0;
+	int n, led = 0, i = 0, arr_i = 0, count = 0, max = -1, max_i = -1; bool check = false;
 	cin >> n;
-	int elements[n]; int leaders[n] = {};
+	int elements[n] = {}; int leaders[n] = {};
 	for (i = 0; i < n; i++){
 		cin >> elements[i];
 		leaders[i] = -1;
 	}
 	while (led < n){
 		for (i = led+1; i<n; i++){
+			if ((led <= max_i) && check){
+				led = max_i + 1;
+				leaders[arr_i] = max_i;
+				arr_i++; max_i = -1; count++; max = -1;check = false;
+				break;
+			}
 			if (elements[led] <= elements[i]){
 				led = i;
 				continue;
 			}
+			else if (elements[led] > elements[i]){
+				if (max <= elements[i]){
+					max = elements[i]; max_i = i;
+				}
+			}
 		}
 		if (i == n){
-			leaders[arrindex] = led;
-			arrindex++;led++;count++;
+			leaders[arr_i] = led;
+			arr_i++;led++;count++; check = true;
 		}
 	}
-	// int count = 0;
+
 	for (i = 0; i < count ; i++){
 		if (i != (count-1))
 			cout << elements[leaders[i]] << " ";
 		else
 			cout << elements[leaders[i]] << endl;
 	}
-	// cout << count << endl;
 	for (i = 0; i < count; i++){
 		if (i == 0)
 			reverse(elements,elements+leaders[i]);
@@ -64,12 +71,15 @@ int main(){
 			cout << elements[i] << " ";
 		else
 			cout << elements[i] << endl;
-	// cout << endl;
 	return 0;
 }
 
 
-// #include <iostream>
+// // Remember, style matters!
+// // While you may rely upon additional functions 
+// // ensure reverse is declared and functions as described
+// #include <iostream> // this is the only header you may use (for cin, cout, and endl)
+// // #include <iostream>
 // using std::cin;
 // using std::cout;
 // using std::endl;
@@ -93,37 +103,25 @@ int main(){
 // }
 
 // int main(){
-// 	int n, led = 0, i = 0, arr_i = 0, count = 0, max = -1, max_i = -1; bool check = false;
+// 	int n, led = 0, i = 0, arrindex = 0, count = 0;
 // 	cin >> n;
-// 	int elements[n] = {}; int leaders[n] = {};
+// 	int elements[n]; int leaders[n] = {};
 // 	for (i = 0; i < n; i++){
 // 		cin >> elements[i];
 // 		leaders[i] = -1;
 // 	}
 // 	while (led < n){
 // 		for (i = led+1; i<n; i++){
-// 			if ((led < max_i) && check){
-// 				led = max_i + 1;
-// 				leaders[arr_i] = max_i;
-// 				arr_i++; max_i = -1; count++; max = -1; check = false;
-// 				break;
-// 			}
 // 			if (elements[led] <= elements[i]){
 // 				led = i;
 // 				continue;
 // 			}
-// 			else if (elements[led] > elements[i]){
-// 				if (max < elements[i]){
-// 					max = elements[i]; max_i = i;
-// 				}
-// 			}
 // 		}
 // 		if (i == n){
-// 			leaders[arr_i] = led;
-// 			arr_i++;led++;count++; check = true;
+// 			leaders[arrindex] = led;
+// 			arrindex++;led++;count++;
 // 		}
 // 	}
-
 // 	// int count = 0;
 // 	for (i = 0; i < count ; i++){
 // 		if (i != (count-1))
--- cppprogramming/main/testrunner.py
+++ cppprogramming/main/testrunner.py
@@ -2,5 +2,5 @@ import random
 
 print(100000)
 for i in range(0,100000):
-	print(random.randint(0,100000), end = " ")
+	print(random.randint(1,100000), end = " ")
 print()
