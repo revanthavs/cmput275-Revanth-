ec1827cfe8c3f6c9bc4a7e75772edb0f1fd38734
REVANTHAVS <atmakurirevanth@gamil.com>
2021-02-15 09:56:38 -0700

    15-02-2021

--- cppprogramming/throwaway/counter.cpp
+++ cppprogramming/throwaway/counter.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+struct Struct_Counter{
+	Struct_Counter(int value);
+	void Increment();
+	void Decrement();
+	int getValue();
+	Struct_Counter* getThis();
+	int value;
+};
+
 class Counter{
 	public:
 		Counter(int value);
@@ -13,6 +22,31 @@ class Counter{
 	private:
 		int value;
 };
+
+
+Struct_Counter::Struct_Counter(int value = 5){
+	this->value = value;
+	return;
+}
+
+void Struct_Counter:: Increment(){
+	this->value++;
+	return;
+}
+
+void Struct_Counter:: Decrement(){
+	this->value--;
+	return;
+}
+
+int Struct_Counter:: getValue(){
+	return this->value;
+}
+
+Struct_Counter* Struct_Counter:: getThis(){
+	return this;
+}
+
 
 Counter::Counter(int value = 5){
 	this->value = value;
@@ -53,5 +87,20 @@ int main(){
 	Counter* value = &counter1;
 	Counter* value1 = counter1.getThis();
 	cout << value << " == " << value1 << endl;
+	Struct_Counter counter2(15);
+	Struct_Counter counter3;
+	cout << counter2.getValue() << " ";
+	counter2.Increment();
+	cout << counter2.getValue() << " ";
+	counter2.Decrement();
+	cout << counter2.getValue() << " ";
+	cout << counter3.getValue() << " ";
+	counter3.Increment();
+	cout << counter3.getValue() << " ";
+	counter3.Decrement();
+	cout << counter3.getValue() << " \n";
+	Struct_Counter* value2 = &counter3;
+	Struct_Counter* value3 = counter3.getThis();
+	cout << value2 << " == " << value3 << endl;
 	return 0;
 }
