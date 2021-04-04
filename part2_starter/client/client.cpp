36e2ad0738076ff73d49baca5fce15590527efb4
REVANTHAVS <atmakurirevanth@gamil.com>
2021-04-04 10:08:05 -0600

    04-04-2021

--- part2_starter/client/client.cpp
+++ part2_starter/client/client.cpp
@@ -4,6 +4,13 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <sys/socket.h>     // socket, bind, listen, accept
+#include <sys/time.h>       // timeval
+#include <arpa/inet.h>      // inet_ntoa, htonl, htons, ntohl, ntohs
+#include <netdb.h>          // getaddrinfo, freeaddrinfo, INADDR_ANY (superset of netinet/in.h)
+#include <cstdlib>          // atoi
+#include <cstring>          // strlen, strcmp
+#include <string>       
 // Add more libraries, macros, functions, and global variables if needed
 
 using namespace std;
@@ -32,8 +39,21 @@ int create_and_open_fifo(const char * pname, int mode) {
 }
 
 int main(int argc, char const *argv[]) {
+    int MSG_SIZE = 1024;
+    int SERVER_PORT = 50000;
+    string server_IP="";
+
     const char *inpipe = "inpipe";
     const char *outpipe = "outpipe";
+
+    if (argc == 3){
+        SERVER_PORT = atoi(argv[1]);
+        server_IP=argv[2];
+    }
+    else{
+        cout << "Enter port number and server IP address: \n";
+        return 0;
+    }
 
     int in = create_and_open_fifo(inpipe, O_RDONLY);
     cout << "inpipe opened..." << endl;
@@ -41,6 +61,213 @@ int main(int argc, char const *argv[]) {
     cout << "outpipe opened..." << endl;
 
     // Your code starts here
+
+
+
+    // 1. Establish a connection with the server
+    struct sockaddr_in my_addr, peer_addr;
+
+    // zero out the structor variable because it has an unused part
+    memset(&my_addr, '\0', sizeof my_addr);
+
+    // Declare socket descriptor
+    int socket_desc;
+
+   // char outbound[BUFFER_SIZE] = {};
+   // char inbound[BUFFER_SIZE] = {};
+
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_desc == -1) {
+        cerr << "Listening socket creation failed!\n";
+        return 1;
+    }
+
+    // Prepare sockaddr_in structure variable
+    peer_addr.sin_family = AF_INET;                         // address family (2 bytes)
+    peer_addr.sin_port = htons(SERVER_PORT);                // port in network byte order (2 bytes)
+                                                            // htons takes care of host-order to short network-order conversion.
+
+    peer_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);     // internet address (4 bytes). INADDR_LOOPBACK is localhost address
+
+    if (connect(socket_desc, (struct sockaddr *) &peer_addr, sizeof peer_addr) == -1) {
+        std::cerr << "Cannot connect to the host!\n";
+        close(socket_desc);
+        return 1;
+    }
+
+    struct timeval timer = {.tv_sec = 1};
+
+    if (setsockopt(socket_desc, SOL_SOCKET, SO_RCVTIMEO, (void *) &timer, sizeof(timer)) == -1) {
+        std::cerr << "Cannot set socket options!\n";
+        close(socket_desc);
+        return 1;
+      }
+
+
+    while(true){
+        bool flag= false;
+        //2. Read coordinates of start and end points from inpipe (blocks until they are selected)
+        char line[MSG_SIZE] = {};
+        char msg_rec[MSG_SIZE]={};
+        //char Out_buff[MSG_SIZE]={};
+        int bytes_written;
+        int bytes_read;
+        string Ack = "A";
+        
+
+        string route_req="";
+
+        route_req+="R";
+
+        bytes_read = read(in, line, MSG_SIZE);
+
+        if (strcmp("Q", line) == 0) {
+            send(socket_desc, line, strlen(line) + 1, 0);
+            break;
+        }
+
+        char * coord[2];
+        int num = 0;
+        for (auto ch : line) {
+            if (ch == ' ') {
+              ++num;
+            }
+            else {
+              coord[num] += ch;
+            }
+        }
+
+        route_req+=" "+to_string(atof(coord[0])*100000);
+        route_req+=" "+to_string(atof(coord[1])*100000);
+
+        num=0;
+
+        bytes_read = read(in, line, MSG_SIZE);
+
+        if (strcmp("Q", line) == 0) {
+            send(socket_desc, line, strlen(line) + 1, 0);
+            break;
+        }
+        for (auto ch : line) {
+            if (ch == ' ') {
+                ++num;
+            }
+            else {
+              coord[num] += ch;
+            }
+        }
+
+        route_req+=" "+to_string(atof(coord[0])*100000);
+        route_req+=" "+to_string(atof(coord[1])*100000);
+
+        // 3. Write to the socket
+        send(socket_desc, route_req.c_str(), route_req.length() + 1, 0);
+
+        int rec_size = recv(socket_desc, msg_rec, MSG_SIZE, 0);
+        if (rec_size == -1) {
+            cout << "Timeout occurred... state reset!\n";
+            continue;
+        }
+
+        num=0;
+        string msg[2];
+        if(msg_rec[0] == 'N'){
+            send(socket_desc, Ack.c_str() , Ack.length()+1, 0);
+            for(auto ch : msg_rec){ 
+                if(ch == ' '){
+                    num++;
+                }
+                else if(ch == '\0'){
+                    break;
+                }
+                else{
+                    msg[num] += ch;
+                }
+            }
+        }
+        else{
+            cout << "Received unexpected message!\n";
+            continue; 
+        }
+        
+
+        int iter=stoi(msg[1]);
+        for(int i=0; i<iter ; i++)
+        {
+            string msg[3];
+            rec_size = recv(socket_desc, msg_rec, MSG_SIZE, 0);
+            if (rec_size == -1) {
+                cout << "Timeout occurred... state reset!\n";
+                flag=true;
+                break;
+            }
+
+            num=0;
+            if(msg_rec[0] == 'W'){
+                for(auto ch : msg_rec){ 
+                    if(ch == ' '){
+                        num++;
+                    }
+                    else if(ch == '\0'){
+                        break;
+                    }
+                    else{
+                        msg[num] += ch;
+                    }
+                }
+            }
+            else{
+                cout << "Received unexpected message!\n";
+                flag=true;
+                break; 
+            }
+
+            string Waypoint="";
+            Waypoint+=to_string( stod(msg[1]) / 100000 ) + " " + to_string( stod(msg[2]) / 100000);
+
+            char Out_buff[Waypoint.length()+1]={};
+
+            int j;
+            for(j = 0; j < Waypoint.length(); j++)
+            {
+                Out_buff[j]=Waypoint[j];
+            }
+            Out_buff[j]='\n';
+
+                
+            bytes_written = write(out, Out_buff , sizeof Out_buff);
+
+            
+            send(socket_desc, Ack.c_str() , Ack.length()+1, 0);
+        }
+
+        if(flag){
+            continue;
+        }
+            
+        rec_size = recv(socket_desc, msg_rec, MSG_SIZE, 0);
+        if (rec_size == -1) {
+            cout << "Timeout occurred... state reset!\n";
+            
+            continue;
+        }
+
+
+        char E= 'E' ;
+
+        if (msg_rec[0] != E){
+            cout << "Received unexpected message!\n";
+            continue;
+          }
+        else{
+            char out_msg[2]={};
+            out_msg[0]=msg_rec[0];
+            out_msg[1]='\n';
+            bytes_written = write(out, out_msg , sizeof out_msg);
+
+        }
+    }
+
 
     // Here is what you need to do:
     // 1. Establish a connection with the server
@@ -52,8 +279,9 @@ int main(int argc, char const *argv[]) {
     // 6. Go to Step 2
     // 7. Close the socket and pipes
 
-    // Your code ends here
 
+    // Your code ends here
+    close(socket_desc);
     close(in);
     close(out);
     unlink(inpipe);
--- part2_starter/server/server.cpp
+++ part2_starter/server/server.cpp
@@ -148,8 +148,7 @@ int main(int argc, char* argv[]) {
     PORT = atoi(argv[1]);
   }
   else{
-    std::cout << "Enter a port number while running\n./server [port]\n";
-    return 1;
+    std::cout << "Enter port number: \n";
   }
 
   struct sockaddr_in my_addr, peer_addr;
@@ -189,15 +188,22 @@ int main(int argc, char* argv[]) {
   conn_socket_desc = accept(lstn_socket_desc, (struct sockaddr *) &peer_addr, &peer_addr_size);
   if (conn_socket_desc == -1){
     std::cerr << "Connection socket creation failed!\n";
-    close(lstn_socket_desc);
-    // Should conform if we need to close the socket descriptor
+
+    // Need to check weather to continue
+    // Need to check if we need to close the socket descriptor
     return 1;
   }
-  
   std::cout << "Connection request accepted from " << inet_ntoa(peer_addr.sin_addr);
   std::cout << ":" << ntohs(peer_addr.sin_port) << "\n";
 
   struct timeval timer = {.tv_sec = 1, .tv_usec = 10000};
+
+  if (setsockopt(conn_socket_desc, SOL_SOCKET, SO_RCVTIMEO, (void *) &timer, sizeof(timer)) == -1) {
+    std::cerr << "Cannot set socket options!\n";
+    close(conn_socket_desc);
+    return 1;
+  }
+
 
   while(true) {
     int rec_size = recv(conn_socket_desc, buffer, BUFFER_SIZE, 0);
@@ -220,9 +226,9 @@ int main(int argc, char* argv[]) {
     }
     char R = 'R';
     if (buffer[0] == R){
-      string p[5];
+      // Need to check this way
+      char *p[5];
       int at = 0;
-      // Need to change it form line to buffer
       for (auto c : line) {
         if (c == ' ') {
           // starting a new string
@@ -260,7 +266,7 @@ int main(int argc, char* argv[]) {
           std::string str = "N ";
           str += std::to_string(path.size());
           send(conn_socket_desc, str.c_str(), str.length() + 1, 0);
-          int rec_size = recv(conn_socket_desc, buffer, BUFFER_SIZE, 0);
+          rec_size = recv(conn_socket_desc, buffer, BUFFER_SIZE, 0);
           if (rec_size == -1){
             std::cout << "Timeout occurred.. still waiting!\n";
             continue;
@@ -277,19 +283,17 @@ int main(int argc, char* argv[]) {
             str += std::to_string(points[v].lon);
             // str += "\n"; need to check if I need to send newline character
             send(conn_socket_desc, Way_point.c_str(), Way_point.length() + 1, 0);
-            int rec_size = recv(conn_socket_desc, buffer, BUFFER_SIZE, 0);
+            rec_size = recv(conn_socket_desc, buffer, BUFFER_SIZE, 0);
             if (rec_size == -1) {
               std::cout << "Timeout occurred.. still waiting!\n";
-              goto Continue_point;
               // Need to find a way to get to ther outer loop
-              // continue;
+              continue;
             }
             else{
               if (buffer[0] != A){
                 std::cout << "Received unexpected message!\n";
-                goto Continue_point;
                 // Need to find a way to get to the outer loop
-                // continue;
+                continue;
               }
             }
           }
@@ -302,8 +306,6 @@ int main(int argc, char* argv[]) {
       std::cout << "Received unexpected message!\n";
       continue;
     }
-    Continue_point:
-      continue;
   }
 
   return 0;
